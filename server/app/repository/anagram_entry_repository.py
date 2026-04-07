a6c0e38bf7c9ffd3d1a51fc0c1480c305b562733
FEvelina <128091832+FEvelina@users.noreply.github.com>
2026-04-08 01:00:53 +0300

    get_by and create queries

--- server/app/repository/anagram_entry_repository.py
+++ server/app/repository/anagram_entry_repository.py
@@ -1,3 +1,19 @@
-from datetime import date
+from datetime import datetime
 from sqlalchemy.orm import Session
-from server.app.models.anagram_entry
+from server.app.models.anagram_entry import AnagramEntry
+
+def get_by_word(db: Session, word: str) -> AnagramEntry | None:
+    return db.query(AnagramEntry).filter(AnagramEntry.word == word).first()
+
+def create(db: Session, word: str, sorted_key_id: int) -> AnagramEntry:
+    entry = AnagramEntry(
+        word=word,
+        sorted_key_id=sorted_key_id,
+        create_at=datetime.now(),
+        encountered=1
+    )
+
+    db.add(entry)
+    db.commit()
+    db.refresh(entry)
+    return entry
